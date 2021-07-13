5a8deb2d6941d06dc83b72db06008c474ae6b3eb
Alfred Xiao <alfred.xiao@qantashotels.com>
2021-07-13 16:33:59 +1000

    added a consumer using a simple listener error handler

---
+++ src/main/java/com/learnkafka/consumer/LibraryEventsConsumerSimpleErrorHandling.java
@@ -0,0 +1,27 @@
+package com.learnkafka.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.service.LibraryEventsService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventsConsumerSimpleErrorHandling {
+
+    @Autowired
+    LibraryEventsService libraryEventsService;
+
+    @KafkaListener(
+            topics = {"library-events"},
+            errorHandler = "simpleKafkaListenerErrorHandler"
+    )
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        log.info("Received ConsumerRecord : {} ", consumerRecord);
+        libraryEventsService.processLibraryEvent(consumerRecord);
+    }
+}
---
+++ src/main/java/com/learnkafka/errorhandling/SimpleKafkaListenerErrorHandler.java
@@ -0,0 +1,17 @@
+package com.learnkafka.errorhandling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SimpleKafkaListenerErrorHandler implements KafkaListenerErrorHandler {
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
+        log.info("in SimpleKafkaListenerErrorHandler.handleError, message is {}, exception is {} ", message, exception);
+        return null;
+    }
+}
